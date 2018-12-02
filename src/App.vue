72c8ddb14b5ada5c3cdde100276d23cd3b6fa3f3
Александр Лямин <gentih.md@gmail.com>
2018-12-02 21:34:04 +0300

    add contour and shadow

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import BadFlower from './components/BadFlover';
+import BadFlower from './components/BadFlower';
 
 export default {
   name: 'app',
@@ -16,15 +16,15 @@ export default {
         return {
             leafs : [
                 { competence_id: 1,  competence_name: "Бюджетирование и финансовое планирование", score: 2, total: 10, total_correct: "2", percent_correct: 50, percent_min: 50, passed: true },
-                { competence_id: 2,  competence_name: "Бюджетирование и финансовое планирование", score: 2, total: 10, total_correct: "2", percent_correct: 20, percent_min: 50, passed: true },
-                { competence_id: 3,  competence_name: "Бюджетирование и финансовое планирование", score: 2, total: 10, total_correct: "2", percent_correct: 20, percent_min: 50, passed: false },
-                { competence_id: 4,  competence_name: "Бюджетирование и финансовое планирование", score: 2, total: 10, total_correct: "2", percent_correct: 20, percent_min: 50, passed: true },
-                { competence_id: 5,  competence_name: "Бюджетирование и финансовое планирование", score: 2, total: 10, total_correct: "2", percent_correct: 80, percent_min: 50, passed: true },
-                { competence_id: 6,  competence_name: "Бюджетирование и финансовое планирование", score: 2, total: 10, total_correct: "2", percent_correct: 20, percent_min: 50, passed: true },
-                { competence_id: 7,  competence_name: "Бюджетирование и финансовое планирование", score: 2, total: 10, total_correct: "2", percent_correct: 20, percent_min: 50, passed: true },
+                { competence_id: 2,  competence_name: "Бюджетирование и финансовое планирование", score: 2, total: 10, total_correct: "2", percent_correct: 60, percent_min: 80, passed: true },
+                { competence_id: 3,  competence_name: "Бюджетирование и финансовое планирование", score: 2, total: 10, total_correct: "2", percent_correct: 35, percent_min: 90, passed: false },
+                { competence_id: 4,  competence_name: "Бюджетирование и финансовое планирование", score: 2, total: 10, total_correct: "2", percent_correct: 50, percent_min: 30, passed: true },
+                { competence_id: 5,  competence_name: "Бюджетирование и финансовое планирование", score: 2, total: 10, total_correct: "2", percent_correct: 45, percent_min: 70, passed: true },
+                { competence_id: 6,  competence_name: "Бюджетирование и финансовое планирование", score: 2, total: 10, total_correct: "2", percent_correct: 38, percent_min: 80, passed: true },
+                { competence_id: 7,  competence_name: "Бюджетирование и финансовое планирование", score: 2, total: 10, total_correct: "2", percent_correct: 80, percent_min: 60, passed: true },
                 { competence_id: 8,  competence_name: "Бюджетирование и финансовое планирование", score: 2, total: 10, total_correct: "2", percent_correct: 60, percent_min: 50, passed: false },
-                { competence_id: 9,  competence_name: "Бюджетирование и финансовое планирование", score: 2, total: 10, total_correct: "2", percent_correct: 20, percent_min: 50, passed: true },
-                { competence_id: 10, competence_name: "Бюджетирование и финансовое планирование", score: 2, total: 10, total_correct: "2", percent_correct: 20, percent_min: 50, passed: true},
+                { competence_id: 9,  competence_name: "Бюджетирование и финансовое планирование", score: 2, total: 10, total_correct: "2", percent_correct: 28, percent_min: 65, passed: true },
+                { competence_id: 10, competence_name: "Бюджетирование и финансовое планирование", score: 2, total: 10, total_correct: "2", percent_correct: 74, percent_min: 60, passed: true},
             ]}
     }
 }
--- src/components/FlowerLeaf.vue
+++ src/components/FlowerLeaf.vue
@@ -1,27 +1,33 @@
 <template>
-    <g :mask="maskAttribute" @click="mouseOver">
-        <circle cx="290.5" cy="313" :r="options.scoreCircleRadius" :fill="options.scoreCircleColor" />
-
-        <circle cx="290.5" cy="313" :r="230" fill="none" stroke="black" stroke-dasharray="9"/>
+    <g :id="'leaf' + options.id">
+        <flower-leaf-contour :number="options.id - 1" :shadow="false"></flower-leaf-contour>
+        <g :mask="maskAttribute">
+            <circle cx="290.5" cy="313" :r="options.scoreCircleRadius" :fill="options.scoreCircleColor" />
+            <circle cx="290.5" cy="313" :r="options.thresholdScore" fill="none" stroke="black" stroke-dasharray="9"/>
+        </g>
     </g>
 </template>
 
 <script>
+    import FlowerLeafContour from './FlowerLeafContour';
     export default {
         name: "FlowerLeaf",
+        components: {
+            FlowerLeafContour
+        },
         props: [
             'options',
             'number'
         ],
         computed: {
             maskAttribute: function () {
-                var index = this.options.id - 1;
+                let index = this.options.id - 1;
                 return 'url(#mask' + index + ')';
             }
         },
         methods: {
             mouseOver: function () {
-                console.log(this.options.id);
+                this.$emit('mouse-over-leaf', this.option.id);
             }
         }
     }
